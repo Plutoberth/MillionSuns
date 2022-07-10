c04058695694c0ad9ffe2535f9fe6d8a8b2e97de
Itay Raveh <itayitay30@gmail.com>
2022-07-10 23:13:26 +0300

    Adjust to `Params` changes

--- params_demo.py
+++ params_demo.py
@@ -1,5 +1,5 @@
 import dash_bootstrap_components as dbc
-from dash import Dash, Input, Output, html
+from dash import Dash
 
 from params import Params
 
@@ -8,105 +8,18 @@ if __name__ == '__main__':
         'start_year': 2020,
         'end_year': 2050,
         'population': {
-            'unit': 'M',
-            'ranges': [
-                {
-                    'enabled': True,
-                    'start_year': 2020,
-                    'end_year': 2050,
-                    'interpo': {
-                        'type': 'constant',
-                        'value': 10
-                    }
-                },
-                {
-                    'enabled': False,
-                    'start_year': 2020,
-                    'end_year': 2050,
-                    'interpo': {
-                        'type': 'constant',
-                        'value': 10
-                    }
-                },
-                {
-                    'enabled': False,
-                    'start_year': 2020,
-                    'end_year': 2050,
-                    'interpo': {
-                        'type': 'constant',
-                        'value': 10
-                    }
-                },
-                {
-                    'enabled': False,
-                    'start_year': 2020,
-                    'end_year': 2050,
-                    'interpo': {
-                        'type': 'constant',
-                        'value': 10
-                    }
-                },
-                {
-                    'enabled': False,
-                    'start_year': 2020,
-                    'end_year': 2050,
-                    'interpo': {
+            'ranges': {
+                'enabled': True,
+                'start_year': 2020,
+                'end_year': 2050,
+                'interpo': {
+                    'selected': {
                         'type': 'constant',
                         'value': 10
                     }
                 }
-            ]
+            },
         },
-        'solar_panel_price': {
-            'unit': 'USD/Kw',
-            'ranges': [
-                {
-                    'enabled': True,
-                    'start_year': 2020,
-                    'end_year': 2050,
-                    'interpo': {
-                        'type': 'constant',
-                        'value': 10
-                    }
-                },
-                {
-                    'enabled': False,
-                    'start_year': 2020,
-                    'end_year': 2050,
-                    'interpo': {
-                        'type': 'constant',
-                        'value': 10
-                    }
-                },
-                {
-                    'enabled': False,
-                    'start_year': 2020,
-                    'end_year': 2050,
-                    'interpo': {
-                        'type': 'constant',
-                        'value': 10
-                    }
-                },
-                {
-                    'enabled': False,
-                    'start_year': 2020,
-                    'end_year': 2050,
-                    'interpo': {
-                        'type': 'constant',
-                        'value': 10
-                    }
-                },
-                {
-                    'enabled': False,
-                    'start_year': 2020,
-                    'end_year': 2050,
-                    'interpo': {
-                        'type': 'constant',
-                        'value': 10
-                    }
-                }
-            ]
-        }
     }
 
     params = Params(**params_json)
@@ -119,39 +32,15 @@ if __name__ == '__main__':
         ],
         external_scripts=[
             'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js',
-        ],
-        suppress_callback_exceptions=True
-    )
-
-    app.layout = html.Div(
-        [
-            params.dash(app),
-            dbc.Button('Show JSON', id='json-btn'),
-            dbc.Collapse(
-                id='collapse',
-                children=[
-                    dbc.Textarea(id='ta', rows=20),
-                    dbc.Button('Update', id='update-btn')
-                ]
-            ),
         ]
     )
 
-
-    @app.callback(
-        Output('collapse', 'is_open'),
-        Input('json-btn', 'n_clicks')
+    app.layout = params.dash_editor(
+        app,
+        'Params',
+        'Enter ranges of interpolated predictions,'
+        ' which will be combined with 2018 data,'
+        ' to simulate energy production and usage in the future.'
     )
-    def update_ta(n_click: int):
-        return n_click and n_click % 2 == 1
-
-
-    @app.callback(
-        Output('ta', 'value'),
-        Input('update-btn', 'n_clicks')
-    )
-    def update_ta(n_click: int):
-        return params.json(indent=4)
-
 
     app.run(debug=True)
