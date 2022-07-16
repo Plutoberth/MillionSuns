69ae78c20108a6229150d62e2f2ff2663bc53d5d
Itay Raveh <itayitay30@gmail.com>
2022-07-16 12:16:03 +0300

    Make `InterpoRange` a `DashListable`

--- params/interpo_range.py
+++ params/interpo_range.py
@@ -23,10 +23,10 @@ Represented by a ``dbc.AccordionItem``.
 """
 from abc import ABC, abstractmethod
 
-from pydantic import PositiveInt, StrictBool
+from pydantic import PositiveInt
 
-from .base_dash_model import DashModel
-from .interpo import InterpoSelect
+from .base_dash_model import DashListable
+from .interpo import Constant, InterpoSelect
 
 
 class ABCInterpoRange(ABC):
@@ -40,11 +40,13 @@ class ABCInterpoRange(ABC):
         ...
 
 
-class InterpoRange(DashModel, ABCInterpoRange):
-    enabled: StrictBool
-    start_year: PositiveInt
-    end_year: PositiveInt
-    interpo: InterpoSelect
+class InterpoRange(DashListable, ABCInterpoRange):
+    start_year: PositiveInt = 2020
+    end_year: PositiveInt = 2050
+    interpo: InterpoSelect = InterpoSelect(__root__=Constant(value=0.0))
+
+    def title(self) -> str:
+        return f'{self.start_year}-{self.end_year} ({self.interpo.selected})'
 
     def at(self, year: int) -> float:
-        return self.interpo.selected.at(self.start_year, self.end_year, year)
+        return self.interpo.__root__.at(self.start_year, self.end_year, year)
