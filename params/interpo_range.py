3613d04a96472debd42818aa87ff0429e5f97600
Itay Raveh <itayitay30@gmail.com>
2022-07-19 01:55:20 +0300

    Add missing `@property` to `InterpoRange`. Go to hell plotly Dash, give me some normal error handling ffs

--- params/interpo_range.py
+++ params/interpo_range.py
@@ -35,6 +35,7 @@ class InterpoRange(DashListable, ABCInterpoRange):
     end_year: PositiveInt = 2050
     interpo: InterpoSelect = InterpoSelect(__root__=Constant(value=0.0))
 
+    @property
     def title(self) -> str:
         return f'{self.start_year}-{self.end_year} ({self.interpo.selected})'
 
