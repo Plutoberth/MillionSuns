4feb0d095523d2f571dc07f5199064cd03a8feb3
Plutoberth <nir61202@gmail.com>
2022-07-28 22:58:32 +0300

    add units and conversions for the units

---
+++ units/units.py
@@ -0,0 +1,13 @@
+from pydantic import NonNegativeFloat, NonNegativeInt
+
+# Kilowatt
+Hour = NonNegativeInt
+kW = NonNegativeFloat
+ILS = NonNegativeFloat
+kg = NonNegativeFloat
+
+kWh = NonNegativeFloat
+ILS_per_kWh = NonNegativeFloat
+ILS_per_kW = NonNegativeFloat
+ILS_per_kg = NonNegativeFloat
+kg_per_kWh = NonNegativeFloat
