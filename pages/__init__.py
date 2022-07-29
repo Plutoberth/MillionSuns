efa70708cba5e33e25104a7ea13d245159a178a9
Plutoberth <nir61202@gmail.com>
2022-07-30 00:10:31 +0300

    organize costs graph

--- pages/__init__.py
+++ pages/__init__.py
@@ -5,7 +5,7 @@ import typing as t
 from dash_models import Page
 from .daily import daily_page
 from .params import params_page
-from .scenarios import scenarios_page
+from .cost_graph import scenarios_page
 
 if t.TYPE_CHECKING:
     from params import AllParams
